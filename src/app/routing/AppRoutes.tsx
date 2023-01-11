0b19063183b196db68ff0e2c4d2532326caaf807
MdMoin788 <developermdmoin@gmail.com>
2023-01-12 02:16:55 +0530

    send

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -18,11 +18,12 @@ import {App} from '../App'
  * @see https://facebook.github.io/create-react-app/docs/using-the-public-folder
  */
 const {PUBLIC_URL} = process.env
+console.log('PUBLIC_URL', PUBLIC_URL);
 
 const AppRoutes: FC = () => {
   const {currentUser} = useAuth()
   return (
-    <BrowserRouter basename={PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route element={<App />}>
           <Route path='error/*' element={<ErrorsPage />} />
